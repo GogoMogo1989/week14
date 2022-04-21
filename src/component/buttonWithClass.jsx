d398c392e2a05345be70e2283460e64260dbbfa9
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-04-21 16:26:22 +0200

    20220421

--- src/component/buttonWithClass.jsx
+++ src/component/buttonWithClass.jsx
@@ -2,14 +2,35 @@ import React, { Component } from 'react';
 
 class ButtonWithClass extends Component {
     constructor() {
-        super() //ezzel kötjük össze a kettő class-t
+        super() //ezzel kötjük össze a kettő class-t, és meghívjuk a két class is egyben
         this.state = { //ezzel mutatunk vissza a leendő objectumra.
             key1:"this is the default state of a class component",
-            key2:0
+            key2:0,
+            title: {
+                main:"main title",
+                sub: "subtitle"
+            },
+            countries: []
         }
-        
+        console.log("1")
     }
+
+    componentDidMount() {
+        console.log("3")
+        fetch('https://restcountries.com/v3.1/all')
+         .then(res => res.json())
+         .then(countriesData => this.setState(
+             () => {
+                 return {countries: countriesData}
+             },
+             () => {
+                 console.log(this.state)
+             }
+         ))
+    }
+
     render() {
+       console.log("2")
         return (
             <div>
                <button onClick={() => this.setState({key1:"heyehuya"})}>
@@ -18,6 +39,41 @@ class ButtonWithClass extends Component {
                <button onClick={() => this.setState({key2:1})}>
                     {this.state.key2}
                </button>
+              
+             {/*    <button onClick={() => {
+                    this.state.key2= 1
+                    console.log(this.state)}}>
+                 
+                    {this.state.key2}
+                </button> */}
+           {/*       <button onClick={() => {
+                     
+                     this.setState({title:{
+                     main:"h1", 
+                     sub:"heye"}})
+                     console.log(this.state)}}>
+                    {this.state.title.main}
+               </button> */}
+               <button onClick={() => {
+                     
+                     this.setState(
+                         () => {
+                             return{
+                                title:{
+                                    main:"h1", 
+                                    sub:"heye"}
+                             }
+                         },
+                         () => {
+                            console.log(this.state)
+                         }
+                     )
+                     }}>
+                    {this.state.title.main}
+               </button>
+               <div className="countries">
+                   {this.state.countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+               </div>
             </div>
         );
     }
--- src/component/buttonWithHook.jsx
+++ src/component/buttonWithHook.jsx
@@ -1,9 +1,20 @@
-import React, { useState} from 'react'
+import React, { useEffect, useState} from 'react'
 
 const ButtonWithHook = () => {
 
-  const [key1, setKey1] = useState("this is the default state of a class component")
-  const [key2, setKey2] = useState(0)
+  const [key1, setKey1] = useState("this is the default state of a class component");
+  const [key2, setKey2] = useState(0);
+  const [countries, setCountries] = useState([]);
+
+         useEffect(
+           () => {
+             console.log("render")
+              fetch('https://restcountries.com/v3.1/all')
+                .then(res => res.json())
+                .then(countriesData => setCountries(countriesData))
+           },
+           []
+         )
 
   return (
     <div>
@@ -13,6 +24,9 @@ const ButtonWithHook = () => {
         <button onClick={() => setKey2(1)}>
             {key2}
         </button>
+        <div className="countries">
+          {countries.map((country, index) => <div key={index}>{country.name.common}</div>)}
+        </div>
     </div>
   )
 }
